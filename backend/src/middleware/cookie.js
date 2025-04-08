09f0af6bee030f4ce9295104a10d1aef838fab22
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2025-04-08 22:20:48 +0530

    fix: deploy fix

--- backend/src/middleware/cookie.js
+++ backend/src/middleware/cookie.js
@@ -1,14 +1,6 @@
 export const cookieConfig = {
   httpOnly: true,
-  // In production you’ll likely need secure cookies and sameSite 'none' (if different domains are involved)
-  secure: false,
-  // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-  samesite: 'none',
-  maxAge: 7 * 24 * 60 * 60 * 1000,
+  secure: process.env.NODE_ENV === 'production', // ✅
+  sameSite: 'None', // ✅ Case-sensitive
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
-// export const cookieConfig = {
-//   httpOnly: true,
-//   secure: process.env.NODE_ENV === 'production',
-//   sameSite: 'strict',
-//   maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
-// };
